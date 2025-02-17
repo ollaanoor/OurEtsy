43a4f07f09298ad95dcc25bc1cbddc0f1c7fa5eb
ollaanoor <ollaanoor65@gmail.com>
2025-02-18 00:41:26 +0200

    added banner and discover gifts and fixed Categories Dropdown btn

--- JS/HomePage.js
+++ JS/HomePage.js
@@ -1,19 +1,16 @@
-// Toggle the dropdown content on click
-document.querySelector('#categories').addEventListener('click', function(event){
-    // Ensure that the click event does not propagate to the window click event
-    event.stopPropagation();
-    document.querySelector('.dropdown-content').classList.toggle('show');
-});
+var dropdowns = document.getElementsByClassName('dropdown')[0];
+
+dropdowns.addEventListener('click', ()=>{
+    var content = document.getElementsByClassName('dropdown-content')[0];
+    content.classList.toggle('show');
+})
 
 // Close the dropdown if the user clicks outside of it
-window.onclick = function(event){
-    if(!event.target.matches('#categories')){
-        var dropdowns = document.getElementsByClassName('dropdown-content');
-        for(var i=0; i<dropdowns.length; i++){
-            var openDropdown = dropdowns[i];
-            if(openDropdown.classList.contains('show')){
-                openDropdown.classList.remove('show');
-            }
+document.addEventListener('click', function(event){
+    if (!event.target.matches('.dropbtn')) {
+        var openDropdown = document.getElementsByClassName("dropdown-content")[0];
+        if(openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
         }
     }
-}
+})
